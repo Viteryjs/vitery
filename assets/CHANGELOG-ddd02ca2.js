685a1b81900d96c4ade5d8da29438cf728cead70
JounQin <admin@1stg.me>
2023-04-08 14:13:53 +0000

    Deploying to gh-pages from @ Viteryjs/vitery@107e5c8cfdc74ae6b0c7f5d35f011199d5a15860 🚀

---
+++ assets/CHANGELOG-ddd02ca2.js
@@ -0,0 +1,9 @@
+import{j as e}from"./index-30927a26.js";function t(i){const n=Object.assign({h1:"h1",p:"p",a:"a",ul:"ul",li:"li"},i.components);return e.jsxs(e.Fragment,{children:[e.jsx(n.h1,{children:"Change Log"}),`
+`,e.jsxs(n.p,{children:[`All notable changes to this project will be documented in this file.
+See `,e.jsx(n.a,{href:"https://conventionalcommits.org",children:"Conventional Commits"})," for commit guidelines."]}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"./packages/@vitery/copy/CHANGELOG.md",children:"@vitery/copy"})}),`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"./packages/@vitery/html/CHANGELOG.md",children:"@vitery/html"})}),`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"./packages/@vitery/svgr/CHANGELOG.md",children:"@vitery/svgr"})}),`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"./packages/vitery/CHANGELOG.md",children:"vitery"})}),`
+`]})]})}function s(i={}){const{wrapper:n}=i.components||{};return n?e.jsx(n,Object.assign({},i,{children:e.jsx(t,i)})):t(i)}export{s as default};
---
+++ assets/README-d71e0a7d.js
@@ -0,0 +1,39 @@
+import{j as e}from"./index-30927a26.js";function c(s){const n=Object.assign({h1:"h1",p:"p",h2:"h2",ul:"ul",li:"li",a:"a",h3:"h3",pre:"pre",code:"code",table:"table",thead:"thead",tr:"tr",th:"th",tbody:"tbody",td:"td",img:"img"},s.components);return e.jsxs(e.Fragment,{children:[e.jsx(n.h1,{children:"vitery"}),`
+`,e.jsx(n.p,{children:"Incredible Vite plugins, make Vite greater to achieve victory"}),`
+`,e.jsx(n.h2,{children:"TOC "}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsxs(n.li,{children:[e.jsx(n.a,{href:"#usage",children:"Usage"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"#install",children:"Install"})}),`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"#api",children:"API"})}),`
+`]}),`
+`]}),`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"#sponsors",children:"Sponsors"})}),`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"#backers",children:"Backers"})}),`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"#changelog",children:"Changelog"})}),`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"#license",children:"License"})}),`
+`]}),`
+`,e.jsx(n.h2,{children:"Usage"}),`
+`,e.jsx(n.h3,{children:"Install"}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{className:"language-sh",children:`# pnpm
+pnpm add vitery
+
+# yarn
+yarn add vitery
+
+# npm
+npm i vitery
+`})}),`
+`,e.jsx(n.h3,{children:"API"}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{className:"language-js",children:`import echo from 'vitery'
+
+echo()
+`})}),`
+`,e.jsx(n.h2,{children:"Sponsors"}),`
+`,e.jsxs(n.table,{children:[e.jsx(n.thead,{children:e.jsxs(n.tr,{children:[e.jsx(n.th,{children:"1stG"}),e.jsx(n.th,{children:"RxTS"}),e.jsx(n.th,{children:"UnTS"})]})}),e.jsx(n.tbody,{children:e.jsxs(n.tr,{children:[e.jsx(n.td,{children:e.jsx(n.a,{href:"https://opencollective.com/1stG",children:e.jsx(n.img,{src:"https://opencollective.com/1stG/organizations.svg",alt:"1stG Open Collective backers and sponsors"})})}),e.jsx(n.td,{children:e.jsx(n.a,{href:"https://opencollective.com/rxts",children:e.jsx(n.img,{src:"https://opencollective.com/rxts/organizations.svg",alt:"RxTS Open Collective backers and sponsors"})})}),e.jsx(n.td,{children:e.jsx(n.a,{href:"https://opencollective.com/unts",children:e.jsx(n.img,{src:"https://opencollective.com/unts/organizations.svg",alt:"UnTS Open Collective backers and sponsors"})})})]})})]}),`
+`,e.jsx(n.h2,{children:"Backers"}),`
+`,e.jsxs(n.table,{children:[e.jsx(n.thead,{children:e.jsxs(n.tr,{children:[e.jsx(n.th,{children:"1stG"}),e.jsx(n.th,{children:"RxTS"}),e.jsx(n.th,{children:"UnTS"})]})}),e.jsx(n.tbody,{children:e.jsxs(n.tr,{children:[e.jsx(n.td,{children:e.jsx(n.a,{href:"https://opencollective.com/1stG",children:e.jsx(n.img,{src:"https://opencollective.com/1stG/individuals.svg",alt:"1stG Open Collective backers and sponsors"})})}),e.jsx(n.td,{children:e.jsx(n.a,{href:"https://opencollective.com/rxts",children:e.jsx(n.img,{src:"https://opencollective.com/rxts/individuals.svg",alt:"RxTS Open Collective backers and sponsors"})})}),e.jsx(n.td,{children:e.jsx(n.a,{href:"https://opencollective.com/unts",children:e.jsx(n.img,{src:"https://opencollective.com/unts/individuals.svg",alt:"UnTS Open Collective backers and sponsors"})})})]})})]}),`
+`,e.jsx(n.h2,{children:"Changelog"}),`
+`,e.jsxs(n.p,{children:["Detailed changes for each release are documented in ",e.jsx(n.a,{href:"./CHANGELOG.md",children:"CHANGELOG.md"}),"."]}),`
+`,e.jsx(n.h2,{children:"License"}),`
+`,e.jsxs(n.p,{children:[e.jsx(n.a,{href:"http://opensource.org/licenses/MIT",children:"MIT"})," © ",e.jsx(n.a,{href:"https://GitHub.com/JounQin",children:"JounQin"}),"@",e.jsx(n.a,{href:"https://www.1stg.me",children:"1stG.me"})]})]})}function t(s={}){const{wrapper:n}=s.components||{};return n?e.jsx(n,Object.assign({},s,{children:e.jsx(c,s)})):c(s)}export{t as default};
